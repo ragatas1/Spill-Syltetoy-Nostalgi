a48aa09f818cfe679de546c85b3c11c18a01ffda
ragatas1 <emil.sonstaberg@gmail.com>
2023-11-16 22:27:05 +0100

    finpuss på movement, spesielt på hoppingen

--- Spill syltetoy Nostalgi/Assets/Scripts/movement.cs
+++ Spill syltetoy Nostalgi/Assets/Scripts/movement.cs
@@ -6,38 +6,66 @@ using UnityEngine.SceneManagement;
 public class movement : MonoBehaviour
 {
     private float horizontal;
-    public float speed = 8f;
-    public float jumpingPower = 16f;
+    public float speed;
+    public float jumpingPower;
     private bool isFacingRight = true;
-    public float spiller1Timer;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
-    private void Start()
-    {
-    }
+    [SerializeField] private float tyngdekraft;
+    [SerializeField] private float hoppeTyngdekraft;
+    [SerializeField] private float bufferTid;
+    [SerializeField] private float coyoteTime;
+    private bool falleTyngdkraft;
+    private bool hoppe;
+    private bool paBakken;
+
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+        if (Input.GetButtonDown("Jump"))
+        {
+            StartCoroutine(Hoppe());
+        }
+        if (IsGrounded()) 
+        {
+            paBakken = true;
+        }
+        else
+        {
+            StartCoroutine (PaBakken());
+        }
+        if (hoppe == true && paBakken == true)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
         }
 
-
-        if (Input.GetButtonUp("Jump") &&  rb.velocity.y > 0f)
+        if (rb.velocity.y > 0 && Input.GetButton("Jump"))
         {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y *0.5f);
+            falleTyngdkraft = true;
         }
+        else
+        {
+            falleTyngdkraft = false;
+        }
+        if (falleTyngdkraft == true)
+        {
+            rb.gravityScale = hoppeTyngdekraft;
+        }
+        else
+        {
+            rb.gravityScale = tyngdekraft;
+        }
+
         flip();
     }
     private void FixedUpdate () 
     {
       rb.velocity = new Vector2 (horizontal * speed, rb.velocity.y);
     }
-    private bool IsGrounded()
+    public bool IsGrounded()
     {
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
@@ -51,5 +79,17 @@ public class movement : MonoBehaviour
             localScale.x *= -1f; 
             transform.localScale = localScale;
         }
+    }
+    private IEnumerator Hoppe()
+    {
+        hoppe = true;
+        yield return new WaitForSeconds(bufferTid);
+        hoppe = false;
+    }
+    private IEnumerator PaBakken()
+    {
+        yield return new WaitForSeconds(coyoteTime);
+        paBakken = false;
+        Debug.Log("coyote ferdig");
     }
 }
